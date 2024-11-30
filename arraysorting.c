17406e8e10b9ccb548bee3fb0324efb1db62b82e
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2024-11-30 21:22:22 +0530

    Create arraysorting.c
    sorting an unsorted array!

---
+++ arraysorting.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n - 1; i++) {
+        // Last i elements are already in place
+        for (j = 0; j < n - i - 1; j++) {
+            // Swap if the element found is greater than the next element
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[10] = {64, 34, 25, 12, 22, 11, 90, 45, 78, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    bubbleSort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    return 0;
+}
