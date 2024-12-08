33183faa97f250c77ba12f8b228e82f8c9967f21
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2024-12-08 21:01:07 +0530

    concatenatestrings.c
    Here we concatenated two strings together!

---
+++ concatenatestrings.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int concatnate(char string1[], char string2[]){
+    while(string1 != '\0'){
+        string1++;
+        length
+    }
+  
+    while(string2 != '\0'){
+        string1[] = string2;
+        string1++;
+        string2++;
+    }
+    return 0;
+}
+
+int main(){
+    char string1[50],string2[50];
+    
+    printf("Give me a string1: ");
+    scanf("%[^\n]%*c",string1);
+    
+        
+    printf("Give me a string2: ");
+    scanf("%[^\n]%*c",string2);
+    
+    concatnate(string1,string2);
+    
+    printf("The concatnate string is: %s\n",string1);
+    
+    return 0;
+}
